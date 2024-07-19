f9227e1ede5702937c9fa99335b6393b1eda04be
Sean Petrie <sean.petrie17@gmail.com>
2024-07-18 22:53:44 -0500

    Updated UI handling

--- BT Vek Mechs/Patches/DeferredEffectExtension.cs
+++ BT Vek Mechs/Patches/DeferredEffectExtension.cs
@@ -2,11 +2,11 @@
 using CustAmmoCategories;
 using EasyLayout;
 using HarmonyLib;
+using Org.BouncyCastle.Utilities.Collections;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
-using Localize;
 using UnityEngine;
 
 namespace VekMechs.Patches;
@@ -14,21 +14,20 @@ namespace VekMechs.Patches;
 static class DeferredEffectExtension {
     private static Regex TriggerAfterActivation = new("TriggerAfterActivation");
 
+    //Update display counts for stuck tagged units
     [HarmonyPatch(typeof(DeferredEffect), nameof(DeferredEffect.RoundsRemain))]
     static class DeferredEffect_RoundsRemain_DefferedEffectTrigger {
 
         [HarmonyPostfix]
         static void Postfix(ref DeferredEffect __instance, ref int __result) {
-            Main.s_log.Log($"DeferredEffect.RoundsRemain");
             if (TriggerAfterActivation.IsMatch(__instance.definition.id)
                 && __instance.ancor != null
             ) {
-                Main.s_log.Log($"DeferredEffect.RoundsRemain = 1");
                 __result = 1;
             }
         }
     }
-
+    //Update display counts for stuck tagged units
     [HarmonyPatch(typeof(DeferredEffect), nameof(DeferredEffect.Init), new Type[] { 
         typeof(Weapon),
         typeof(DeferredEffectDef),
@@ -46,6 +45,7 @@ static class DeferredEffectExtension {
             __instance.CountDownFloatie.Text.SetText(def.text + ":" + roundsText.ToString());
         }
     }
+    //Trigger tagged effects after a mech activates
     [HarmonyPatch(typeof(AbstractActor), nameof(AbstractActor.OnActivationEnd))]
     static class AbstractActor_OnActivationEnd_DefferedEffectTrigger {
         
@@ -58,7 +58,6 @@ static class DeferredEffectExtension {
                 if (TriggerAfterActivation.IsMatch(deferredEffect.definition.id)
                     && ancor == deferredEffect.ancor 
                 ) {
-                    Main.s_log.Log($"{ancor?.name} Matched");
                     hashSet.Add(deferredEffect);
                     deferredEffect.PlayEffect();
                     deferredEffect.gameObject.SetActive(true);
@@ -67,6 +66,25 @@ static class DeferredEffectExtension {
             foreach (DeferredEffect item in hashSet) {
                 DeferredEffectHelper.deferredEffects.Remove(item);
                 DeferredEffectHelper.playingEffects.Add(item);
+            }
+        }
+    }
+    //Detach when a mech dies so that it reverts back to normal behavior
+    [HarmonyPatch(typeof(AbstractActor), nameof(AbstractActor.FlagForDeath))]
+    static class AbstractActor_FlagForDeath_DefferedEffectTrigger {
+
+        [HarmonyPostfix]
+        static void Postfix(ref AbstractActor __instance) {
+            Transform ancor = __instance.GameRep?.transform;
+
+            foreach (DeferredEffect deferredEffect in DeferredEffectHelper.deferredEffects) {
+                if (TriggerAfterActivation.IsMatch(deferredEffect.definition.id)
+                    && ancor == deferredEffect.ancor
+                ) {
+                    deferredEffect.ancor = null;
+                    deferredEffect.offset = ancor.position;
+                    deferredEffect.CountDownFloatie.Text.SetText(deferredEffect.definition.text + ":" + deferredEffect.RoundsRemain(DeferredEffectHelper.CurrentRound).ToString());
+                }
             }
         }
     }
