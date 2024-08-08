cf78154bb17ff60815846a4be17971ed4a02e25d
Sean Petrie <sean.petrie17@gmail.com>
2024-08-08 17:10:56 -0500

    Updated names and descriptions

--- BT Vek Mechs/Main.cs
+++ BT Vek Mechs/Main.cs
@@ -1,10 +1,13 @@
-﻿using HarmonyLib;
+﻿using BattleTech.Data;
+using HarmonyLib;
+using HBS.Extensions;
 using HBS.Logging;
+using UnityEngine;
 
 namespace VekMechs;
 
 public static class Main {
-    public static readonly ILog s_log = Logger.GetLogger(nameof(VekMechs));
+    public static readonly ILog s_log = HBS.Logging.Logger.GetLogger(nameof(VekMechs));
     public static void Start() {
         s_log.Log("Starting");
 
@@ -22,5 +25,26 @@ public static class Main {
     static void GetReleaseVersion(ref string __result) {
         var old = __result;
         __result = old + "\nHTWHX(2)";
+    }
+
+    [HarmonyPatch(typeof(DataManager), nameof(DataManager.PooledInstantiate))]
+    static class DeferredEffect_RoundsRemain_DefferedEffectTrigger {
+
+        [HarmonyPostfix]
+        static void Postfix(ref GameObject __result, string id) {
+            switch (id) {
+                case "chrPrfWeap_riflemaniic_rightarm_laser_eh3":
+                    __result.FindFirstChildNamed("jm6_right_arm_forearm_uac5_bh2").transform.DetachChildren();
+                    break;
+                case "chrPrfWeap_riflemaniic_rightarm_laser_eh1":
+                    __result.FindFirstChildNamed("jm6_right_arm_forearm_uac5_bh1").transform.DetachChildren();
+                    break;
+                case "chrPrfWeap_riflemaniic_leftarm_laser_eh1":
+                    __result.transform.localPosition = new Vector3(0.13f, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
